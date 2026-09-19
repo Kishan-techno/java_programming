3970b701c5f18ca1aa4db42b469ab35685d76704
Kishan kumar <kishankumarmbi07@gmail.com>
2026-09-19 21:18:24 +0530

    ry

---
+++ note.java
@@ -0,0 +1,12 @@
+public class note {
+
+    public static void main(String[] args) {
+
+        // Input
+        int n = 5;
+
+        int note = n * 4;
+
+        System.out.println(note);
+    }
+}
